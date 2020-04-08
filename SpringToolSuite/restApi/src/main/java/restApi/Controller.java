98a46d04234a97323c7b90a2ee10397bf4342783
priyamvad <priyamvad123@gmail.com>
2020-04-08 10:19:14 +0530

    completed 11 out of 16 advertisement assignment

--- SpringToolSuite/restApi/src/main/java/restApi/Controller.java
+++ SpringToolSuite/restApi/src/main/java/restApi/Controller.java
@@ -9,11 +9,15 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -94,7 +98,7 @@ public class Controller {
 			return null;
 		}
 	}
-	@PostMapping(value="/logout")
+	@PostMapping(value="/logout",produces=MediaType.APPLICATION_JSON_VALUE)
 	public String logOut(@RequestHeader(name="authToken") String authToken)
 	{
 		if(authToken==null)
@@ -110,6 +114,111 @@ public class Controller {
 		else
 		{
 			return "LogIn or register to continue";
+		}
+	}
+	public UserRestApi checkLoggedInUser(String authToken)
+	{
+
+		if(authToken==null)
+		{
+			return null;
+		}
+		if(loggedUserMap.containsKey(authToken))
+		{	
+			UserRestApi user=loggedUserMap.get(authToken);
+			return user;
+		}
+		else
+		{
+			return null;
+		}
+
+	}
+	@PutMapping(value="/updateadv",consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public String updateAdvertisementOfUser(@RequestBody Advertisement advertisement,@RequestHeader(name="authToken") String authToken)
+	{
+		UserRestApi user=checkLoggedInUser(authToken);
+		if(user==null)
+		{
+			return "LogIn or register to continue";
+		}
+		else
+		{	
+			
+			if(advertisement!=null &&((!advertisement.getName().equals(""))||advertisement.getName()!=null))
+			{	
+				if(user.getAdvertisementList().stream().filter((Advertisement adv)->adv.equals(advertisement)).findAny().isPresent())
+				{
+					Advertisement updateAdvertisement=user.getAdvertisementList().stream().filter((Advertisement adv)->adv.equals(advertisement)).collect(Collectors.toList()).get(0);
+					user.deleteAdvertisementList(updateAdvertisement);
+					user.setAdvertisementList(advertisement);
+					return "Advertisement updated succesfully";
+				}
+				else
+				{
+					return "No advertisement of name "+ advertisement.getName()+" is present";
+				}
+			}
+			else
+			{
+				return "advertisement "+ advertisement+" should have a valid name";
+			}
+			
+		}
+	}
+	@GetMapping(value = "/getpartadvofuser",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Advertisement> getAUserParticularAdvertisementLsit(@RequestHeader(name="authToken") String authToken,@RequestParam(name="advname") String advName)
+	{	
+		UserRestApi user=checkLoggedInUser(authToken);
+		if(user == null)
+		{
+			return null;
+		}
+		else
+		{
+			if(advName==null||advName.equals(""))
+			{
+				return null;
+			}
+			else
+			{
+				return user.getAdvertisementList().stream().filter((Advertisement advertisement)->advertisement.getName().equals(advName)).collect(Collectors.toList());
+			}
+		}
+	}
+	@DeleteMapping(value = "/delpartadvofuser/{advname}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public String deleteAUserParticularAdvertisementLsit(@RequestHeader(name="authToken") String authToken,@PathVariable(name="advname") String advName)
+	{	
+		UserRestApi user=checkLoggedInUser(authToken);
+		if(user == null)
+		{
+			return "Log in to continue";
+		}
+		else
+		{
+			if(advName==null||advName.equals(""))
+			{
+				return "Give a valid advertisement name";
+			}
+			else
+			{
+				Advertisement adv= user.getAdvertisementList().stream().filter((Advertisement advertisement)->advertisement.getName().equals(advName)).collect(Collectors.toList()).get(0);
+				user.deleteAdvertisementList(adv);
+				return "advertisement adv "+adv+" from user "+user+" deleted succesfully";
+			}
+		}
+	}
+	@GetMapping(value = "/getdetailsofuser",produces=MediaType.APPLICATION_JSON_VALUE)
+	public String getAUserDetails(@RequestHeader(name="authToken") String authToken)
+	{	
+		UserRestApi user=checkLoggedInUser(authToken);
+		if(user == null)
+		{
+			return "Log in to continue";
+		}
+		else
+		{
+			return user.toString();
 		}
 	}
 			
